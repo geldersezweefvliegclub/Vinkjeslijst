c95d94a03ae1f1cb73f6f4750a112b7255d99ff4
rjnkr <richard@jnkr.eu>
2026-04-25 21:08:28 +0200

    24 uurs loggings

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import {TwentyFourHourLogger} from "./twenty-four-hour-logger";
 
 async function bootstrap() {
-  const app = await NestFactory.createApplicationContext(AppModule);
+  const app = await NestFactory.createApplicationContext(AppModule, {
+    logger: new TwentyFourHourLogger(),
+  });
   const logger = new Logger('Application');
 
   logger.log('Application started as background worker');
